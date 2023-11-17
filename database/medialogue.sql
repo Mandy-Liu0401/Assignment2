592c174042f55d9e2f2560dcf0c6c2249679808e
Mengying Liu <liu00624@algonquinlive.com>
2023-11-17 17:59:52 -0500

    update ddl

---
+++ database/medialogue.sql
@@ -0,0 +1,52 @@
+-- Database: `booklogue` and php web application user
+CREATE DATABASE booklogue;
+GRANT USAGE ON *.* TO 'appuser'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON medialogue.* TO 'appuser'@'localhost';
+FLUSH PRIVILEGES;
+
+USE booklogue;
+
+-- Table structure for table `users`
+CREATE TABLE IF NOT EXISTS 'users' (
+    'id' int(8) NOT NULL AUTO_INCREMENT,
+    'username' varchar(40) NOT NULL,
+    'email' varchar(255) NOT NULL,
+    'password' varchar(255) NOT NULL,
+    PRIMARY KEY ('id')
+) 
+
+CREATE TABLE IF NOT EXISTS 'books' (
+    'id' int(8) NOT NULL AUTO_INCREMENT,
+    'title' varchar(255) NOT NULL,
+    'authour' varchar(255),
+    'genre' varchar(255),
+    'description' TEXT,
+    PRIMARY KEY ('id')
+)
+
+INSERT INTO 'users' ('id', 'username', 'email','password') VALUES
+(1, 'test', 'test@gmail.com', 'pass1');
+
+INSERT INTO 'books' ('id', 'title', 'email','password') VALUES
+(1, 'test', 'test@gmail.com', 'pass1');
+
+
+-- CREATE TABLE IF NOT EXISTS 'movies' (
+--     'id' int(8) NOT NULL AUTO_INCREMENT,
+--     'title' varchar(255) NOT NULL,
+--     'genre' varchar(255),
+--     'rating' varchar(255),
+--     'description' TEXT,
+--     'Runtime' varchar(255),
+--     PRIMARY KEY ('id')
+-- )
+
+-- CREATE TABLE IF NOT EXISTS 'games' (
+--     'id' int(8) NOT NULL AUTO_INCREMENT,
+--     'title' varchar(255) NOT NULL,
+--     'genre' varchar(255),
+--     'rating' varchar(255),
+--     'description' TEXT,
+--     'publisher' varchar(255),
+--     PRIMARY KEY ('id')
+
