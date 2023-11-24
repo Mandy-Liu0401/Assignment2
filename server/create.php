135b8d888f12cb8908b9879e4b14209133227763
Mengying Liu <liu00624@algonquinlive.com>
2023-11-24 13:17:43 -0500

    update

--- server/create.php
+++ server/create.php
@@ -13,20 +13,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { //check is the data has submitted
     $description = $_POST['description'];
     $comment = $_POST['comment'];
 
-    if (isset($_FILES['imagePath'])) {
+     // Set a default value for $file_path
+    $file_path = "../images/default.png";
+
+    if (isset($_FILES['imagePath'])&& $_FILES['imagePath']['error'] == UPLOAD_ERR_OK) {//check if a file uploded successfully
         $file_name = $_FILES['imagePath']['name'];
         $file_tmp = $_FILES['imagePath']['tmp_name'];
-        move_uploaded_file($file_tmp, "../images/" . $file_name);
-    } else {
-        $file_name = 'default.png';
-    }
+        $file_path = "../images/".$file_name;
+        move_uploaded_file($file_tmp, $file_path);
+    } 
+    
     $sql = "INSERT INTO books (title, author, genre, description, comment, imagePath) 
-        VALUES ('$title','$author','$genre', '$description', '$comment','../images/$file_name')";
+        VALUES ('$title','$author','$genre', '$description', '$comment','$file_path')";
 
     $result = mysqli_query($db, $sql);
 
     $id = mysqli_insert_id($db);
-    //redirect to show page
+    //redirect to viewBook page
     header("Location: viewBook.php?id=$id");
 
 
